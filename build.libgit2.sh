41bb9a974914d90f1a8c6999d0834ea33a9b5aeb
BaY1251 <48309205+BaY1251@users.noreply.github.com>
2024-03-23 16:42:24 +0800

    Update build.libgit2.sh

--- build.libgit2.sh
+++ build.libgit2.sh
@@ -39,7 +39,7 @@ if [[ $RID == android-* ]]; then
                     -DCMAKE_SYSTEM_VERSION=24 \
                     -DCMAKE_ANDROID_ARCH_ABI=$ABI \
                     -DCMAKE_ANDROID_NDK=$ANDROID_NDK_HOME \
-                    -DCMAKE_ANDROID_STL_TYPE=none \
+                    -DCMAKE_ANDROID_STL_TYPE=gabi++_shared \
                     "
     echo $CMAKE_ANDROID
 fi
